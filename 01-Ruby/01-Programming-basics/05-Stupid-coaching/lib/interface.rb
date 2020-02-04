400faa90115583ff279ffa648b57fe0402afb823
AnnKoh <annkoh12@gmail.com>
2020-02-04 16:00:59 +0800

    coach

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,17 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Hello there."
+input = gets.chomp
+
+while input != "I am going to work right now!"
+  if input == input.upcase
+    if input == "I AM GOING TO WORK RIGHT NOW!"
+      break
+    else
+      puts coach_answer_enhanced(input)
+    end
+  else
+    puts coach_answer(input)
+  end
+  input = gets.chomp
+end
