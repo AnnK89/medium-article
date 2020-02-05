46ebaa9c95542a814f3d6159ebfa798677fc5b25
AnnKoh <annkoh12@gmail.com>
2020-02-05 16:02:18 +0800

    1984 GEORGE ORWELL

--- 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,11 +1,14 @@
 def bugged_concatenate(array)
   concatenated_string = ""
   array.each do |element|
+    element = element.to_s
     concatenated_string = concatenated_string + element
   end
   return concatenated_string.upcase
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  bugged_concatenate([1, "9", 84, " ", "George Orwell"])
 end
+
+p build_1984_title
