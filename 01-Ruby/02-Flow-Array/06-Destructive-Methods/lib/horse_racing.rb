e8dad4a31e32df8bb3ce2fb67fe2cafde2560521
AnnKoh <annkoh12@gmail.com>
2020-02-05 18:28:03 +0800

    checking rake failure

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,10 +1,11 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
 
-  race_array.map! do |horsename|
+  race_array.map! do |horsenameS|
     "#{race_array.index(horsename) + 1}-#{horsename}!"
   end
   race_array.reverse!
 end
 
 # puts horse_racing_format!(["hi", "there"])
+# Hello I am a new line of code
