6417f0e72078a3499adec4b233165ee3717e9bcb
AnnKoh <annkoh12@gmail.com>
2020-02-05 15:44:50 +0800

    Black Jack

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -2,8 +2,21 @@ require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
   # TODO: return (not print!) a message containing the player's score and bank's score
+  return "Your score is #{player_score}, bank is #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
+  case
+  when player_score == 21
+    return "Black jack! Yon won!"
+  when player_score > 21
+    return "You lost"
+  when player_score > bank_score
+    return "You won"
+  when player_score < bank_score
+    return "You lost"
+  when player_score == bank_score
+    return "Push"
+  end
 end
--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -2,3 +2,19 @@ require_relative "croupier"
 
 # TODO: make the user play from terminal in a while loop that will stop
 #       when the user will not be asking for  a new card
+
+puts "Hello there. Card? 'y' or 'yes' to get a new card"
+user_input = gets.chomp
+
+score = 0
+bank = pick_bank_score
+
+while ["y", "yes"].include? user_input
+  # user_input == 'y' || user_input == 'yes'
+  score = pick_player_card + score
+  puts state_of_the_game(score, bank)
+  puts "Card? 'y' or 'yes' to get a new card"
+  user_input = gets.chomp
+end
+
+puts end_game_message(score, bank)
