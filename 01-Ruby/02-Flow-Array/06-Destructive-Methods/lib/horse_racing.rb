82c6b7ae8258e076fe9e8761a1fd2ddbf584d03a
AnnKoh <annkoh12@gmail.com>
2020-02-05 18:17:31 +0800

    Horse racing

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,7 +1,10 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+
+  race_array.map! do |horsename|
+    "#{race_array.index(horsename) + 1}-#{horsename}!"
+  end
+  race_array.reverse!
 end
 
-
-
-
+# puts horse_racing_format!(["hi", "there"])
