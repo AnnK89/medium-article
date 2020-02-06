ce65d2795234d20a7112e42dcdebe52fd831a5a6
AnnKoh <annkoh12@gmail.com>
2020-02-07 01:18:58 +0800

    louchebem

--- 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/06-Louchebem/lib/louchebem.rb
@@ -1,3 +1,30 @@
-def louchebemize(sentence)
+def louchebemize_word(word)
   # TODO: implement your louchebem translator
+  random_suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"].sample
+  return word if word.size == 1
+
+  if ["a", "e", "i", "o", "u"].include? word[0]
+    "l#{word}#{random_suffix}"
+  else
+    first_vowel_index = word.chars.index { |letter| ["a", "e", "i", "o", "u"].include? letter }
+    beginning = word[0...first_vowel_index]
+    middle = word[first_vowel_index...word.length]
+    "l#{middle}#{beginning}#{random_suffix}"
+  end
 end
+
+def louchebemize(sentence)
+  result_elements = []
+  words = sentence.split(/\b/)
+
+  words.each do |word|
+    if word =~ /\W/
+      result_elements << word
+    else
+      result_elements << louchebemize_word(word)
+    end
+  end
+  result_elements.join
+end
+
+# p louchebemize("Hello who are you")
