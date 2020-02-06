d7fab5fba6d339bab104f64d4facc823de7dad85
AnnKoh <annkoh12@gmail.com>
2020-02-06 17:41:26 +0800

    Splitter

--- 01-Ruby/03-Iterators-Blocks/04-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/04-Splitter/lib/splitter.rb
@@ -2,6 +2,10 @@ def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+  new_array = []
+  new_array << array.select { |word| word.size == size }.sort
+  new_array << array.reject { |word| word.size == size }.sort
+  return new_array
 end
 
 def block_splitter(array)
@@ -9,4 +13,20 @@ def block_splitter(array)
   #       elements for which the given block yields true,
   #       the second containing all the other elements.
   #       No sort needed this time.
+  new_array = []
+  new_array << array.select { |name| yield(name) }
+  new_array << array.reject { |name| yield(name) }
+  return new_array
 end
+
+
+# words = %w(dog data ask my win two beer as)
+# p result = size_splitter(words, 3)
+
+# result => [["ask", "dog", "two", "win"], ["as", "beer", "data", "my"]]
+
+# beatles = [ "John", "Paul", "Ringo", "George" ]
+
+# result = block_splitter(beatles) { |beatle| beatle.start_with?("P") }
+
+# # result => [ [ "Paul" ], [ "John", "Ringo", "George" ] ]
