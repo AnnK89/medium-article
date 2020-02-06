60844b6953915f9ba3ef4c8aac9f0fec3c22fb74
AnnKoh <annkoh12@gmail.com>
2020-02-06 17:43:47 +0800

    modified google code

--- 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/03_tag.rb
+++ 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/03_tag.rb
@@ -2,10 +2,10 @@ def tag(tag_name, attributes = nil)
   # TODO: Build HTML tags around content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
   if attributes.nil?
-    return "<#{tag_name}>#{yield}</#{tag_name}>"
+    p "<#{tag_name}>#{yield}</#{tag_name}>"
   else
     # return "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{yield}</#{tag_name}>"
-    return "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{yield}</#{tag_name}>"
+    p "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{yield}</#{tag_name}>"
   end
 end
 
@@ -20,9 +20,14 @@ end
 # end
 # => '<a href="www.google.com">Google it</a>'
 
-# tag("a", ["href", "www.google.com"]) do
+# tag_end = tag("a", ["href", "www.google.com"]) do
+#   puts "Entering the first block"
 #   tag("h1") do
+#     # puts "Entering the second block"
 #     "Google it"
+#     # puts "Finishing the first block"
 #   end
 # end
-# => '<a href="www.google.com"><h1>Google it</h1></a>'
+# # => '<a href="www.google.com"><h1>Google it</h1></a>'
+
+# puts tag_end
