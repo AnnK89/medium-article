c3318000226559b5bf1005439b5a4631c3099edf
AnnKoh <annkoh12@gmail.com>
2020-02-07 23:27:00 +0800

    Roman to Integer

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,3 +1,26 @@
+ROMAN_INT =
+  {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+
 def roman_to_integer(roman_string)
-  # TODO: translate roman string to integer
+  sum = 0
+  int_num = roman_string.split("").map { |x| ROMAN_INT[x.to_sym] }
+  # ["X", "I", "I"], get value by using key ROMAN_INT["X".to_sym], return an array of values
+  int_num.each_with_index do |num, index| # iterate through [10, 1, 1]
+    if index + 1 == int_num.size
+      sum += num
+    elsif int_num[index] >= int_num[index + 1]
+      sum += num
+    else
+      sum -= num
+    end
+  end
+  sum
 end
