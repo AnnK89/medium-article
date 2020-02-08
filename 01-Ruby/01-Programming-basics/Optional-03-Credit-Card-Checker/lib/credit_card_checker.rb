68916ceb0f268df3204e54843ddc42ffbbddb16b
AnnKoh <annkoh12@gmail.com>
2020-02-08 20:37:09 +0800

    Credit card checker

--- 01-Ruby/01-Programming-basics/Optional-03-Credit-Card-Checker/lib/credit_card_checker.rb
+++ 01-Ruby/01-Programming-basics/Optional-03-Credit-Card-Checker/lib/credit_card_checker.rb
@@ -1,11 +1,25 @@
 def visa?(card)
   # TODO: A visa card starts with a 4
+  card.delete(" ").start_with? "4"
 end
 
 def mastercard?(card)
   # TODO: A mastercard card starts with a 5
+  card.delete(" ").start_with? "5"
 end
 
 def valid_card?(card)
   # TODO: Implement the validator. Return true if the card is valid, false otherwise.
+  sum = 0
+  digits = card.delete(" ").split("").map { |digit| digit.to_i } # an array of integers
+  return false if digits.empty?
+
+  digits.each_with_index do |digit, index|
+    if index.even?
+      sum += digit * 2 >= 10 ? (digit * 2) - 9 : digit * 2
+    else
+      sum += digit
+    end
+  end
+  (sum % 10).zero?
 end
