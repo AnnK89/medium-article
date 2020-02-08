53316271769cec5472876b26fd29bafa652ccf8c
AnnKoh <annkoh12@gmail.com>
2020-02-09 00:26:26 +0800

    Morse code

--- 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/decoder.rb
+++ 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/decoder.rb
@@ -1,3 +1,42 @@
 def decode(morse_text)
   # TODO: Decode the morse text you just got!
+  morse_text.downcase.split("|").map { |morse_word| decode_word(morse_word) }.join(" ")
 end
+
+def decode_word(morse_word)
+  morse_word.split(" ").map { |morse_letter| MORSE_CODE.key(morse_letter) }.join.upcase
+end
+
+
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+
+
+
+p decode(".... ..|--. ..- -.-- ...")
--- 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/encoder.rb
+++ 01-Ruby/01-Programming-basics/Optional-04-Morse-Code/lib/encoder.rb
@@ -1,4 +1,45 @@
 def encode(text)
   # TODO: write a method which returns the morse sentence for the given text
   # NOTE: you may want to extract a `encode_word` method
+  words = text.downcase.gsub(/[,.!?']/, "").split(" ")
+  words.map { |word| encode_word(word) }.join("|")
 end
+
+def encode_word(word)
+  word.chars.map { |alphabet| MORSE_CODE[alphabet] }.join(" ")
+end
+
+
+MORSE_CODE = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+}
+
+p MORSE_CODE.key("--..")
+
+
+# p encode("Hey Jude, don't make it bad")
