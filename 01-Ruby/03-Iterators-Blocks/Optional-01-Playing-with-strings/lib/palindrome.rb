fb0fe5ae48687acdc186a956e664c2433254c771
AnnKoh <annkoh12@gmail.com>
2020-02-09 10:01:04 +0800

    Palindrome

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,11 @@
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
+  return false if a_string.empty?
+
+  a_string = a_string.downcase.delete("- ,!'")
+  a_string == a_string.reverse
 end
+
+# p palindrome?("wagon")
+# p palindrome?("Laval")
+# p palindrome?("Madam, I'm Adam!")
