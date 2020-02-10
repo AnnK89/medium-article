5fd2fab19369a13d71a4cf66085375cc99573ebb
AnnKoh <annkoh12@gmail.com>
2020-02-10 13:45:06 +0800

    Reboot: Calculator

--- 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
@@ -1,1 +1,30 @@
 # TODO: you can build your calculator program here!
+
+def calculation(operator, first, second)
+  "Result: #{first.send(operator.to_sym, second)} "
+end
+
+def ask_for_input(ordinal)
+  puts "Enter a #{ordinal} number:"
+  print "> "
+  variable = gets.chomp.to_f
+end
+
+puts "Hello there!"
+
+while true
+
+  first_num = ask_for_input("first")
+  second_num = ask_for_input("second")
+
+  puts "Choose an operation: [+][-][*][/]"
+  print "> "
+  user_operation = gets.chomp
+
+
+  puts calculation(user_operation, first_num, second_num)
+
+  puts "Do you want to calculate again? [Y/N]"
+  break if gets.chomp == "N"
+
+end
