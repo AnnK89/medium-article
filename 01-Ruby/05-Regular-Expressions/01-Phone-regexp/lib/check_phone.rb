d48e4c7c4dd0836eb165f46860ebc245eb46f809
AnnKoh <annkoh12@gmail.com>
2020-02-10 18:40:36 +0800

    French number regex

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,9 @@
+REGEX_1 = /^\+33[1-9]\d{8}/
+REGEX_2 = /^0[1-9]{8}/
+
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number = phone_number.gsub(/\s|-/, "")
+
+  (phone_number.match? REGEX_1) || (phone_number.match? REGEX_2)
 end
