2774746ff4ce37f3164348f09e77e71479ea6c8e
AnnKoh <annkoh12@gmail.com>
2020-02-11 00:06:48 +0800

    Regex: anagrams

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,7 +1,29 @@
+def normalized_string(string)
+  string.gsub(/\W/, "").downcase
+end
+
 def anagrams?(a_string, another_string)
-  # TODO: implement the obvious method to test if two words are anagrams
+  normalized_string(a_string).chars.sort.join == normalized_string(another_string).chars.sort.join
+end
+
+def char_frequencies(string)
+  normalized_string(string).chars.reduce(Hash.new(0)) do |hash, char|
+    hash[char] += 1
+    hash
+  end
 end
 
 def anagrams_on_steroids?(a_string, another_string)
-  # TODO: implement the improved method
+  return false if a_string.length != another_string.length
+
+  a_frequencies = char_frequencies(a_string)
+  another_frequencies = char_frequencies(another_string)
+  a_frequencies.each_pair do |letter, frequency|
+    return false if another_frequencies[letter] != frequency
+  end
+  true
 end
+
+p anagrams_on_steroids?("Monica Lewinsky!", "Nice silky woman")
+p anagrams_on_steroids?("aladdin", "aladin")
+p anagrams_on_steroids?("aladdin", "alladin")
