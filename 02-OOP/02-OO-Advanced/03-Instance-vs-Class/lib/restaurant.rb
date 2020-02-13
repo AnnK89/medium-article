04e919fe62930a88b6f21aed3d2a224d173b4a79
AnnKoh <annkoh12@gmail.com>
2020-02-13 11:44:20 +0800

    Restaurant: Instance vs Class

--- 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,23 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :city
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @ratings << new_rate
+  end
+
+  def average_rating
+    @ratings.reduce(:+).fdiv(@ratings.length)
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant| restaurant.city == city }
+  end
 end
