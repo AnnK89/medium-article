333dad8618e9a0572d250650b27c9861fa0e3261
AnnKoh <annkoh12@gmail.com>
2020-02-22 21:36:52 +0800

    Anagrams

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,3 +1,14 @@
+require 'pry-byebug'
+
 def group_anagrams(words)
-  # TODO: group anagrams
+  groups = {}
+  words.each do |word|
+    crash_word = word.downcase.chars.sort.join
+    if groups[crash_word]
+      groups[crash_word] << word
+    else
+      groups[crash_word] = [word]
+    end
+  end
+  groups.values
 end
