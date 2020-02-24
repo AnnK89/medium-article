46764f7c89591f77a2bbc8ea93062787efe4834f
AnnKoh <annkoh12@gmail.com>
2020-02-24 13:44:59 +0800

    Faker

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,3 +1,11 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+
+100.times do
+  post = Post.new(
+    title: Faker::Games::Pokemon.name,
+    url: "www.#{Faker::Games::Pokemon.location}.com"
+  )
+  post.save
+end
