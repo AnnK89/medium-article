a47077b21b386ef33496a2f6427ee523601cd3df
AnnKoh <annkoh12@gmail.com>
2020-02-24 10:51:46 +0800

    db:migrate

--- 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |post|
+      post.string :title
+      post.string :url
+      post.timestamps
+    end
   end
 end
