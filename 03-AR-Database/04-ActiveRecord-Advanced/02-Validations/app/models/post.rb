478fe5010b6f9ed498179597f7f4e307b356f7fc
AnnKoh <annkoh12@gmail.com>
2020-02-25 13:33:54 +0800

    Validations

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :name, length: { minimum: 5 }
+  validates :url, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /http:\/\/.*\z/ }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
+  before_validation :strip_whitespace
+  after_validation :send_email
 
-  # TODO: Add some validation
-  # TODO: Add some callbacks
+  def strip_whitespace
+    self.email = email.strip unless email.nil?
+  end
+
+  def send_email
+    FakeMailer.instance.mail('boris@lewagon.org', 'Welcome to HN!')
+  end
 end
