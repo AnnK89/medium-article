571405c51c86312380c4e80ea9fce2446541881a
AnnKoh <annkoh12@gmail.com>
2020-02-25 11:34:43 +0800

    Associations

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,22 @@
 # TODO: Write a seed
+require 'Faker'
+
+5.times do
+  user = User.new(
+    username: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+  user.save!
+end
+
+User.all.each do |user|
+  rand(5..10).times do
+    post = Post.new(
+      name: Faker::Movie.quote,
+      url: "www.#{Faker::Company.name}.com",
+      votes: rand(0..10)
+    )
+    post.user = user
+    post.save!
+  end
+end
