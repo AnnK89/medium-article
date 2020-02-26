ef25e97fa6e6d4920f966ed7dd083503e392a986
AnnKoh <annkoh12@gmail.com>
2020-02-26 17:54:26 +0800

    cleaning up

--- 03-AR-Database/02-SQL-CRUD/Optional-01-My-First-ORM/app/models/record.rb
+++ 03-AR-Database/02-SQL-CRUD/Optional-01-My-First-ORM/app/models/record.rb
@@ -1,7 +1,49 @@
 # You can use a global variable, DB, which
 # is an instance of SQLite3::Database
 # NO NEED TO CREATE IT, JUST USE IT.
+require 'pry-byebug'
 
 class Record
-  # TODO: all the code should be here!
+  def initialize(attrs = {})
+    @id = attrs[:id]
+  end
+
+  def self.find(id)
+    DB.results_as_hash = true
+    table = self.to_s.downcase + "s"
+    result = DB.execute("SELECT * FROM #{table} WHERE id = ?", id)
+    return nil if result.empty?
+
+    args = result[0].transform_keys(&:to_sym)
+    new_instance = self.new
+    # binding.pry
+    args.each_pair do |k, v|
+      new_instance.instance_variable_set("@#{k}", v)
+      self.attr_reader k
+    end
+    new_instance
+  end
+
+  def self.all
+    DB.results_as_hash = true
+    table = self.to_s.downcase + "s"
+    results = DB.execute("SELECT * FROM #{table}")
+
+    sym_keys = results.map do |result|
+      result.transform_keys(&:to_sym)
+    end
+
+    sym_keys.each_pair do |k, v|
+      self.new.instance_variable_set("@#{k}", v)
+      self.attr_reader k
+    end
+
+  end
+
+
+  def save
+  end
+
+  def destroy
+  end
 end
