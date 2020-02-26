d6619272b5a3a228d209090753f65709f2b5c2ea
AnnKoh <annkoh12@gmail.com>
2020-02-26 17:46:58 +0800

    committing changes

--- 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,29 @@
 def beer_song(beer_start_count)
   # TODO: sing the song
+  i = beer_start_count
+  while i > 0
+    print_remaining_bottle(i)
+    i -= 1
+  end
 end
+
+def print_remaining_bottle(number)
+  # if number > 1
+  puts "#{bottles(number)} of beer on the wall, #{bottles(number)} of beer!"
+  # else
+  #   puts "#{bottles(number)} of beer on the wall, #{bottles(number)} of beer!"
+  # end
+  puts "Take one down, pass it around, #{bottles(number - 1)} of beer on the wall!"
+end
+
+def bottles(number)
+  if number > 1
+    "#{number} bottles"
+  elsif number == 1
+    "1 bottle"
+  else
+    "no more bottles"
+  end
+end
+
+# p beer_song(ARGV[0].to_i)
